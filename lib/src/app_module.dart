9914a6bdc79a43814ac65796ecb0253d84d69190
GABRIEL  BRAZ <gabrielbrazdev@gmail.com>
2023-05-08 14:54:07 -0300

    no message

---
+++ lib/src/app_module.dart
@@ -0,0 +1,13 @@
+import 'package:dw10_modular/src/modules/home/home_module.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppModule extends Module {
+  @override
+  List<Bind> get binds => [];
+
+  @override
+  List<ModularRoute> get routes => [
+        // ChildRoute('/', child: (context, args) => const HomePage()),
+        ModuleRoute('/home', module: HomeModule())
+      ];
+}
